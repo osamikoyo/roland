29cf74cb2a04c42c9d76312354859823a9af81c7
osamikoyo <v.pylayev@mail.ru>
2026-02-05 06:53:09 +0500

    update config

--- config/config.go
+++ config/config.go
@@ -4,4 +4,5 @@ type Config struct {
 	ModelFile  string `yaml:"model_file"`
 	LLMLibPath string `yaml:"llm_lib_path"`
 	ResponseLength int32 `yaml:"response_length"`
+	CMDS map[string]map[string]string `yaml:"cmds"`
 }
