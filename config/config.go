6d18bbdf0fbcecfbb47be78c11819b1fbdd977ff
osamikoyo <v.pylayev@mail.ru>
2026-02-14 06:20:30 +0500

    update config

--- config/config.go
+++ config/config.go
@@ -1,8 +1,14 @@
 package config
 
+type LLMs struct{
+	Parser string `yaml:"parser"`
+	Listener string `yaml:"listener"`
+}
+
 type Config struct {
-	ModelFile      string                       `yaml:"model_file"`
-	LLMLibPath     string                       `yaml:"llm_lib_path"`
+	LLMs LLMs `yaml:"llms"`
+	LLamaLibPath     string                       `yaml:"llm_lib_path"`
+
 	ResponseLength int32                        `yaml:"response_length"`
 	CMDS           map[string]map[string]string `yaml:"cmds"`
 	SaveCMDOutput  bool                         `yaml:"save_cmd_output"`
