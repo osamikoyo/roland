a49c29f5f17aa99ad9c79dbf4af57e513f004976
osamikoyo <v.pylayev@mail.ru>
2026-02-18 06:58:13 +0500

    update core

--- core/core.go
+++ core/core.go
@@ -1,20 +1,35 @@
 package core
 
 import (
+	"fmt"
 	"roland/config"
 	"roland/logger"
 	"roland/parser"
 	"roland/router"
 	"roland/ui"
+
+	"go.uber.org/zap"
 )
 
-type Core struct{
+type Core struct {
 	inbound chan string
-	parser *parser.Parser
-	router *router.WorkerRouter
-	window *ui.Window
+	parser  *parser.Parser
+	router  *router.WorkerRouter
+	window  *ui.Window
 }
 
-func NewCore(cfg *config.Config, logger *logger.Logger) (*Core, error) {
+func NewCore(cfg *config.Config, logger *logger.Logger, inbound chan string) (*Core, error) {
+	logger.Info("setup parser")
+
+	parser, err := parser.NewParser(cfg, logger)
+	if err != nil {
+		logger.Error("failed setup parser",
+			zap.Error(err))
+
+		return nil, fmt.Errorf("failed setup parser: %w", err)
+	}
+
+	router := router.NewWorkerRouter(logger)
+
 	
 }
