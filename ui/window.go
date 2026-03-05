1d1c34dafb4657ac1beb0ad1832c56466090120a
osamikoyo <v.pylayev@mail.ru>
2026-03-05 06:25:10 +0500

    update window

--- ui/window.go
+++ ui/window.go
@@ -1,11 +1,13 @@
 package ui
 
 import (
+	"fmt"
 	"io"
-	"roland/entity/session"
 	"roland/logger"
+	"strings"
 
-	tea "github.com/charmbracelet/bubbletea"
+	tea "charm.land/bubbletea/v2"
+	"go.uber.org/zap"
 )
 
 type Window struct {
@@ -25,18 +27,68 @@ func NewWindow(logger *logger.Logger) *Window {
 }
 
 func (w *Window) SetQuery(query string) {
-	w.ui.Query = query
+	w.ui.query = query
 }
 
 func (w *Window) IsListening() bool {
 	return w.ui.listening
 }
 
-func (w *Window) NewSession(key string, stderr, stdout io.ReadWriter) {
-	w.ui.sessions[key] = &session.Session{
-		StdErr: stderr,
-		StdOut: stdout,
+func (w *Window) NewSession(key string, stderr, stdout io.ReadWriter) error {
+	w.ui.Tabs = append(w.ui.Tabs, key)
+
+	var builder strings.Builder
+
+	out, err := io.ReadAll(stdout)
+	if err != nil {
+		w.logger.Error("failed read from command stdout",
+			zap.Error(err))
+
+		return fmt.Errorf("failed read from command stdout %w", err)
 	}
 
+	builder.Write(out)
+
+	cerr, err := io.ReadAll(stderr)
+	if err != nil {
+		w.logger.Error("failed read from command stderr",
+			zap.Error(err))
+
+		return fmt.Errorf("failed read from command stderr %w", err)
+	}
+
+	builder.Write(cerr)
+
+	w.ui.TabContent = append(w.ui.TabContent, builder.String())
+
 	w.programm.Send(nil)
+
+	return nil
+}
+
+func (w *Window) CloseSession(key string) {
+	tabs := make([]string, len(w.ui.Tabs)-1)
+	contents := make([]string, len(w.ui.TabContent)-1)
+
+	index := 0
+
+	 j := 0
+
+	for i, tab := range w.ui.Tabs{
+		if tab == key {
+			index = i
+		} else {
+			tabs[j] = tab
+			j++
+		}
+	}
+
+	j = 0
+
+	for i, content := range w.ui.TabContent {
+		if i != index {
+			contents[j] = content
+			j++ 
+		}
+	}
 }
